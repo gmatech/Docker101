3c0786a55ccc5c14aa027b4aaf52919b7ad39794
Gerald Aden (Mac - OSX) <gerald.aden@gmatech.com>
2016-03-21 21:18:28 -0700

    Continue Web app and added catalog service

--- apps/ecapp/Controllers/CatalogController.cs
+++ apps/ecapp/Controllers/CatalogController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using ecapp.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +11,18 @@ namespace ecapp.Controllers
 {
     public class CatalogController : Controller
     {
-        // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            var products = GetProducts();
+            return View(products);
+        }
+        
+        public List<Product> GetProducts()
+        {
+            return new List<Product> {
+                new Product {Id = 1, Name="Product 1", Price=2.95m},
+                new Product {Id = 2, Name="Product 2", Price=4.95m}
+            };
         }
     }
 }
